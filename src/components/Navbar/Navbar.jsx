cd54aacc003cec99d2d7a660c641ccc8824f5c12
sonisumit7904 <sonisumit7904@gmail.com>
2024-10-16 03:23:52 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,42 +9,42 @@ const Navbar = ({ grouping, sorting, onGroupingChange, onSortingChange }) => {
   };
   return (
     <nav className="navbar">
-      <div className="displaySettings tag feature-tag">
-        <button className="display-btn" onClick={toggleDropdown}>
-          <img src="./Display.svg" alt="Display" className="icon" />
-          Display
-          <img src="./down.svg" alt="Display" className="dropdown-arrow" />
-        </button>
+      {/* <div className="displaySettings tag feature-tag"> */}
+      <button className="display-btn" onClick={toggleDropdown}>
+        <img src="./Display.svg" alt="Display" className="icon" />
+        Display
+        <img src="./down.svg" alt="Display" className="dropdown-arrow" />
+      </button>
 
-        {/* Dropdown content */}
-        {isOpen && (
-          <div className="dropdown-content">
-            <div className="dropdown-row">
-              <label>Grouping:</label>
-              <select
-                id="grouping"
-                value={grouping}
-                onChange={(e) => onGroupingChange(e.target.value)}
-              >
-                <option value="status">Status</option>
-                <option value="user">User</option>
-                <option value="priority">Priority</option>
-              </select>
-            </div>
-            <div className="dropdown-row">
-              <label>Ordering:</label>
-              <select
-                id="sorting"
-                value={sorting}
-                onChange={(e) => onSortingChange(e.target.value)}
-              >
-                <option value="priority">Priority</option>
-                <option value="title">Title</option>
-              </select>
-            </div>
+      {/* Dropdown content */}
+      {isOpen && (
+        <div className="dropdown-content">
+          <div className="dropdown-row">
+            <label>Grouping:</label>
+            <select
+              id="grouping"
+              value={grouping}
+              onChange={(e) => onGroupingChange(e.target.value)}
+            >
+              <option value="status">Status</option>
+              <option value="user">User</option>
+              <option value="priority">Priority</option>
+            </select>
           </div>
-        )}
-      </div>
+          <div className="dropdown-row">
+            <label>Ordering:</label>
+            <select
+              id="sorting"
+              value={sorting}
+              onChange={(e) => onSortingChange(e.target.value)}
+            >
+              <option value="priority">Priority</option>
+              <option value="title">Title</option>
+            </select>
+          </div>
+        </div>
+      )}
+      {/* </div> */}
     </nav>
   );
 };
